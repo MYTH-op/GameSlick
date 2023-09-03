0d6a5fd64a071e34ded36a39b22aedbc50575b73
MyTH.op <93129280+MYTH-op@users.noreply.github.com>
2023-09-03 16:17:55 +0530

    TicTac

---
+++ GameSlick/Assets/Scripts/TicTacToe/GridSpace.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+
+    public void SetSpace()
+    {
+        buttonText.text = playerSide;
+        button.interactable = false;
+    }
+}
